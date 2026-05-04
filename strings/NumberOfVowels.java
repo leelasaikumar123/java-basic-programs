5023b03bf68db245aa70f5e121616a7217816063
leelasaikumar123 <leelasaikumarmeda501@gmail.com>
2026-05-04 15:21:43 +0530

    feat:add number of vowels

---
+++ strings/NumberOfVowels.java
@@ -0,0 +1,19 @@
+package strings;
+
+import java.util.Scanner;
+
+public class NumberOfVowels {
+public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+    String s=sc.nextLine();
+    s=s.toLowerCase();
+    int vowels=0;
+    for(int i=0;i<s.length();i++){
+ char c=s.charAt(i);
+ if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u'){
+    vowels++;
+ }
+    }
+    System.out.println(vowels);
+}
+}
